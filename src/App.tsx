d5d66411d377eff79ab17894c547a1178ab0e5f1
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-20 16:04:30 +0200

    Initialise router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <></>;
-}
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={} />)
+);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
