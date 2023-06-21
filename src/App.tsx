c850ec9f9c60e2d9994a1ecec712e8e5c9ead608
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-21 14:30:59 +0200

    Add signup route

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,15 @@ import {
 } from "react-router-dom";
 import Login from "./pages/Login";
 import { AppContextProvider } from "./AppContext";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Login />} />)
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Route>
+  )
 );
 
 const App = () => (
