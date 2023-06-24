4f5a40c5f6f4356bdeb212a8c42c6ec5d2986f7b
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-24 09:17:39 +0200

    Render header element inside of Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-3 items-center px-3 py-2">
+    <header className="grid grid-cols-3 items-center px-3 py-2">
       <div></div>
       <div>
         <h1 className="text-center font-brush text-lg">Friendstagram</h1>
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="flex justify-end">
         <Button icon={<BsChatLeft />} />
       </div>
-    </div>
+    </header>
   );
 };
 
