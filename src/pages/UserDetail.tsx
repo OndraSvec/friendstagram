818fc3f9b4f6da6e3fc78f2baeb62d6f0e0d03bc
Ondra Svec <ondrasvecveslo@gmail.com>
2023-07-02 12:33:42 +0200

    Add userDetail info div

--- src/pages/UserDetail.tsx
+++ src/pages/UserDetail.tsx
@@ -1,8 +1,12 @@
-import { getProfileFeed } from "../firebase/functions";
+import { getProfileFeed, getUser } from "../firebase/functions";
 import ImageGrid from "../components/ImageGrid";
 import { Timestamp } from "firebase/firestore";
 import Wrapper from "../components/Wrapper";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../AppContext";
+import Button from "../components/Button";
+import { FaUserCircle } from "react-icons/fa";
 
 export function loader({ params }) {
   return getProfileFeed(params.userID);
@@ -18,6 +22,53 @@ const UserDetail = () => {
     url: string;
     id: string;
   }[] = useLoaderData();
+  const { userID } = useParams();
+  const [displayedUser, setDisplayedUser] = useState<{
+    email: string;
+    name: string | null;
+    photo: string | null;
+    uid: string;
+  } | null>(null);
+  const { user } = useContext(AppContext);
+
+  useEffect(() => {
+    getUser(userID).then((res) => setDisplayedUser(res));
+  }, []);
+
+  const userInfo = (
+    <div className="mb-2 flex w-full flex-col gap-2 px-1 py-2 sm:gap-3 md:gap-4 lg:w-3/4 lg:gap-6">
+      <div className="flex w-2/3 items-end justify-between self-center sm:w-1/2">
+        <div className="flex flex-col items-center">
+          {displayedUser?.photo ? (
+            <img
+              src={displayedUser?.photo}
+              className="w-9 rounded-full sm:w-12 md:w-[75px] lg:w-[90px]"
+            />
+          ) : (
+            <FaUserCircle className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl" />
+          )}
+          {displayedUser?.name ? (
+            <p className="text-xs sm:text-sm md:text-base lg:text-lg">
+              {displayedUser?.name}
+            </p>
+          ) : (
+            <p className="text-xs sm:text-sm md:text-base lg:text-lg">
+              {displayedUser?.email}
+            </p>
+          )}
+        </div>
+        <p className="text-xs sm:text-sm md:text-base lg:text-lg">
+          Posts: {posts.length}
+        </p>
+      </div>
+      {user.uid !== userID && (
+        <Button
+          text="Send message"
+          className="w-2/3 self-center rounded-md bg-sky-400 p-1 text-xs font-medium text-white disabled:bg-gray-300 sm:w-1/2 sm:p-2 sm:text-sm md:p-3 md:text-base lg:text-lg"
+        />
+      )}
+    </div>
+  );
 
   const feedElements = posts.map((post) => (
     <div key={post.id} className="aspect-square w-full">
@@ -32,6 +83,7 @@ const UserDetail = () => {
   ));
   return (
     <Wrapper>
+      {userInfo}
       <ImageGrid>{feedElements}</ImageGrid>
     </Wrapper>
   );
