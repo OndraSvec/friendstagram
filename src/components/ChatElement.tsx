9415e2a32f3234a03929f0c6ee541f8061740bec
Ondra Svec <ondrasvecveslo@gmail.com>
2023-07-03 11:07:30 +0200

    Add ChatElement text-overflow-ellipsis property

--- src/components/ChatElement.tsx
+++ src/components/ChatElement.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
 import { User } from "firebase/auth";
 import { Timestamp } from "firebase/firestore";
+import { IoArrowRedoSharp } from "react-icons/io5";
+import { IoArrowUndoSharp } from "react-icons/io5";
 
 interface ChatElementProps {
   chat: {
@@ -33,20 +35,22 @@ const ChatElement: React.FC<ChatElementProps> = ({ chat, user }) => {
 
   useEffect(() => {
     const unsub = getUser(userID).then((res) => setChatUser(res));
+
+    return () => unsub;
   }, []);
 
   return (
     <>
       {chatUser && (
         <Link to={`${chat.id}`} className="w-full">
-          <div className="flex w-full items-center justify-between gap-1 whitespace-nowrap p-2 text-xs sm:text-sm md:text-base lg:text-lg">
+          <div className="flex w-full items-center justify-between gap-1 overflow-hidden whitespace-nowrap p-2 text-xs sm:text-sm md:text-base lg:text-lg">
             {chatUser.photo ? (
               <img
                 src={chatUser.photo}
-                className="w-9 rounded-full sm:w-12 md:w-[75px] lg:w-[90px]"
+                className="w-9 flex-shrink-0 rounded-full sm:w-12 md:w-[75px] lg:w-[90px]"
               />
             ) : (
-              <FaUserCircle className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl" />
+              <FaUserCircle className="flex-shrink-0 text-4xl sm:text-5xl md:text-6xl lg:text-7xl" />
             )}
             <div className="flex flex-grow flex-col items-center">
               {chatUser.name ? (
@@ -56,7 +60,16 @@ const ChatElement: React.FC<ChatElementProps> = ({ chat, user }) => {
               )}
               {chat.messages.length > 0 ? (
                 <>
-                  <p>{chat.messages.at(-1)?.message}</p>
+                  <div className="flex w-11/12 items-center gap-1">
+                    {chat.messages.at(-1)?.senderID === userID ? (
+                      <IoArrowUndoSharp />
+                    ) : (
+                      <IoArrowRedoSharp />
+                    )}
+                    <p className="overflow-hidden text-ellipsis">
+                      {chat.messages.at(-1)?.message}
+                    </p>
+                  </div>
                   <p>{chat.updatedAt?.toDate().toUTCString()}</p>
                 </>
               ) : (
